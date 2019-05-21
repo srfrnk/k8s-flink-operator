5a8c34862edddad421e485c42b00a4bf1081cdd3
srfrnk <srfrnk@gmail.com>
2019-05-21 20:47:50 +0300

    fix: report healthy while job not started  -fixes #1

--- job-app/check.sh
+++ job-app/check.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-jobId=$(cat /app/jobId)
-echo "Checking job ${jobId}..."
-count=$(flink list -m ${jobManagerUrl} -r | grep -e ": ${jobId} :" | wc -l)
-if [[ ${count} -gt 0 ]]
-then exit 0
-else exit 1
+echo "Checking health:"
+if [[ -f /app/jobId ]]
+then
+    jobId=$(cat /app/jobId)
+    echo "Checking job ${jobId}..."
+    count=$(flink list -m ${jobManagerUrl} -r | grep -e ": ${jobId} :" | wc -l)
+    if [[ ${count} -gt 0 ]]
+        then echo "Job running.";exit 0
+        else echo "Job missing.";exit 1
+    fi
+else
+    echo "Job not yet started!"
+    exit 0 # Job has not yet started - report as healthy (Issue #1)
 fi
